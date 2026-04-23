afdc1ed2eac2b03f824fc0b061129f2e736f91af
André Sousa <fv.seguros.it@gmail.com>
2026-04-23 14:35:37 +0000

    API Fast as F***

--- main.py
+++ main.py
@@ -519,5 +519,4 @@ def regression_test(
 # -------------------------------
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="127.0.0.1", port=8000)
-    #-- uvicorn.run("main:app", host="0.0.0.0", port=10000)
+    uvicorn.run("main:app", host="0.0.0.0", port=10000)
